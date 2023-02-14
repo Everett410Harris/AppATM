0b9fa9fc4b4cc66da568522efdc806f32b334d98
EverettHarris <EverettHarris@BULLDAWG502>
2023-02-13 22:45:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+public class cardHolder
+{
+    int id;
+    string cardNum;
+    string firstName;
+    string lastName;
+    double balance;
+
+    public cardHolder(int id, string cardNum, string firstName, string lastName, double balance)
+    {
+        this.id = id;
+        this.cardNum = cardNum;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+
+    public getCardNum
+}
